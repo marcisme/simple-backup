3465a496ae2ee3380bd549a0f11c79bf4cb01757
Marc Schwieterman <marc@marcisme.com>
2010-08-23 22:22:24 -0400

    changed file formatting from tabs to spaces

--- simple-backup.sh
+++ simple-backup.sh
@@ -32,8 +32,8 @@
 ##
 
 if [ ! -f ~/.backuprc ]; then
-	echo "You must create .backuprc in your home directory"
-	exit 1
+    echo "You must create .backuprc in your home directory"
+    exit 1
 fi
 
 source ~/.backuprc
@@ -76,48 +76,48 @@ MYSQL_ARCHIVE_FILE=$ARCHIVE_DIR/$MYSQL_ARCHIVE_FILE_NAME
 DAY_OF_WEEK=$(date +%u)
 
 usage() {
-	echo "Usage: $(basename $0) [options]"
-	echo "  -h      help, this message"
-	echo "  -c      print configuration"
-	echo "  -v      verbose output"
-	echo "  -p      pretend, don't execute commands"
-	echo "  -b      full backup (file system and database)"
-	echo "  -s      rsync of backup files to local host"
-	echo "  -f      backup file system"
+    echo "Usage: $(basename $0) [options]"
+    echo "  -h      help, this message"
+    echo "  -c      print configuration"
+    echo "  -v      verbose output"
+    echo "  -p      pretend, don't execute commands"
+    echo "  -b      full backup (file system and database)"
+    echo "  -s      rsync of backup files to local host"
+    echo "  -f      backup file system"
     echo "  -o      force full file system backup"
-	echo "  -d      backup database"
+    echo "  -d      backup database"
 }
 
 validate_env_vars() {
-	if [ -z "$BACKUP_NAME" ]; then
-		echo "BACKUP_NAME is required"
-		exit 1
-	fi
-	
-	if [ -z "$MYSQL_USER" ]; then
-		echo "MYSQL_USER is required"
-		exit 1
-	fi
-	
-	if [ -z "$MYSQL_PASSWORD" ]; then
-		echo "MYSQL_PASSWORD is required"
-		exit 1
-	fi
-	
-	if [ -z "$REMOTE_USER" ]; then
-		echo "REMOTE_USER is required"
-		exit 1
-	fi
-	
-	if [ -z "$REMOTE_HOST" ]; then
-		echo "REMOTE_HOST is required"
-		exit 1
-	fi
-	
-	if [ -z "$LOCAL_DIR" ]; then
-		echo "LOCAL_DIR is required"
-		exit 1
-	fi
+    if [ -z "$BACKUP_NAME" ]; then
+        echo "BACKUP_NAME is required"
+        exit 1
+    fi
+    
+    if [ -z "$MYSQL_USER" ]; then
+        echo "MYSQL_USER is required"
+        exit 1
+    fi
+    
+    if [ -z "$MYSQL_PASSWORD" ]; then
+        echo "MYSQL_PASSWORD is required"
+        exit 1
+    fi
+    
+    if [ -z "$REMOTE_USER" ]; then
+        echo "REMOTE_USER is required"
+        exit 1
+    fi
+    
+    if [ -z "$REMOTE_HOST" ]; then
+        echo "REMOTE_HOST is required"
+        exit 1
+    fi
+    
+    if [ -z "$LOCAL_DIR" ]; then
+        echo "LOCAL_DIR is required"
+        exit 1
+    fi
 }
 
 print_configuration() {
@@ -136,29 +136,29 @@ print_configuration() {
 
 # Force a full backup if it's currently FULL_DAY_OF_WEEK.
 force_full_backup_if_needed() {
-	if [ "$DAY_OF_WEEK" = "$FULL_DAY_OF_WEEK" -o "$DO_FORCE_FULL" ]; then
-		if [ "$VERBOSE" ]; then
-			echo "Removing $INCREMENTAL_FILE to force full backup"
-		fi
-		if [ ! "$PRETENDING" ]; then
-			rm -f $INCREMENTAL_FILE
-		fi
-	fi
+    if [ "$DAY_OF_WEEK" = "$FULL_DAY_OF_WEEK" -o "$DO_FORCE_FULL" ]; then
+        if [ "$VERBOSE" ]; then
+            echo "Removing $INCREMENTAL_FILE to force full backup"
+        fi
+        if [ ! "$PRETENDING" ]; then
+            rm -f $INCREMENTAL_FILE
+        fi
+    fi
 }
 
 # Validate needed directories and create them if needed and possible.
 validate_directories() {
-	if [ ! -d "$DIR_TO_BACKUP" ]; then
-		echo "DIR_TO_BACKUP: ${DIR_TO_BACKUP} does not exist"
-		exit 1
-	fi 
+    if [ ! -d "$DIR_TO_BACKUP" ]; then
+        echo "DIR_TO_BACKUP: ${DIR_TO_BACKUP} does not exist"
+        exit 1
+    fi 
 
-	if [ ! -d "$ARCHIVE_DIR" ]; then
-		echo "Creating backup dir: $ARCHIVE_DIR"
-		if [ ! "$PRETENDING" ]; then
-			mkdir -p $ARCHIVE_DIR
-		fi
-	fi
+    if [ ! -d "$ARCHIVE_DIR" ]; then
+        echo "Creating backup dir: $ARCHIVE_DIR"
+        if [ ! "$PRETENDING" ]; then
+            mkdir -p $ARCHIVE_DIR
+        fi
+    fi
 }
 
 # Backup the configured directory to a tarball. The ARCHIVE_DIR is
@@ -167,87 +167,87 @@ validate_directories() {
 # DEREFERENCE is 1. A snapshot archive is stored in ARCHIVE_DIR for
 # incremental backups. Removing the file will result in a full backup.
 backup_file_system() {
-	local exclude_file_arg
-	local dereference_arg
+    local exclude_file_arg
+    local dereference_arg
 
-	force_full_backup_if_needed
+    force_full_backup_if_needed
 
-	# create --exclude-from if file exists
-	if [ -f "$EXCLUDE_FILE" ]; then
-		exclude_file_arg="--exclude-from $EXCLUDE_FILE"
-	fi
+    # create --exclude-from if file exists
+    if [ -f "$EXCLUDE_FILE" ]; then
+        exclude_file_arg="--exclude-from $EXCLUDE_FILE"
+    fi
 
-	# create --dereference if option set
-	if [ "$DEREFERENCE" -eq 1 ]; then
-		if [ "$VERBOSE" ]; then
-			echo "Symbolic links will be resolved to target files"
-		fi
-		dereference_arg="--dereference"
-	fi
+    # create --dereference if option set
+    if [ "$DEREFERENCE" -eq 1 ]; then
+        if [ "$VERBOSE" ]; then
+            echo "Symbolic links will be resolved to target files"
+        fi
+        dereference_arg="--dereference"
+    fi
 
-	# display backup type
-	if [ "$DO_FORCE_FULL" -o ! -f "$INCREMENTAL_FILE" ]; then
-		echo "Executing full backup"
-	else
-		echo "Executing partial backup"
-	fi
+    # display backup type
+    if [ "$DO_FORCE_FULL" -o ! -f "$INCREMENTAL_FILE" ]; then
+        echo "Executing full backup"
+    else
+        echo "Executing partial backup"
+    fi
 
-	if [ ! "$PRETENDING" ]; then
-		$TAR --create \
-			--file $FS_ARCHIVE_FILE \
-			--listed-incremental $INCREMENTAL_FILE \
-			--exclude $ARCHIVE_DIR \
-			$exclude_file_arg \
-			$dereference_arg \
-			--gzip \
-			--directory $DIR_TO_BACKUP \
-			.
-	fi
+    if [ ! "$PRETENDING" ]; then
+        $TAR --create \
+            --file $FS_ARCHIVE_FILE \
+            --listed-incremental $INCREMENTAL_FILE \
+            --exclude $ARCHIVE_DIR \
+            $exclude_file_arg \
+            $dereference_arg \
+            --gzip \
+            --directory $DIR_TO_BACKUP \
+            .
+    fi
 }
 
 # Backup all databases that the configured user has access to.
 # The user will at a minimum need SELECT and LOCK TABLES privileges.
 backup_database() {
-	echo "Dumping mysql databases"
-	if [ ! "$PRETENDING" ]; then
-		$MYSQLDUMP --all-databases -u$MYSQL_USER -p$MYSQL_PASSWORD | \
-			gzip > $MYSQL_ARCHIVE_FILE
-	fi
+    echo "Dumping mysql databases"
+    if [ ! "$PRETENDING" ]; then
+        $MYSQLDUMP --all-databases -u$MYSQL_USER -p$MYSQL_PASSWORD | \
+            gzip > $MYSQL_ARCHIVE_FILE
+    fi
 }
 
 # Remove any backup files that are older than REMOTE_RETENTION_DAYS.
 remove_old_backups() {
-	local archive_dir=$1
-	local retention_days=$2
-	echo "Removing files older than $retention_days days from $archive_dir"
-	if [ ! "$PRETENDING" ]; then
-		find $archive_dir -name *.gz -mtime +${retention_days} -exec rm -v {} \;
-	elif [ -d "$archive_dir" ]; then
-		find $archive_dir -name *.gz -mtime +${retention_days}
-	fi
+    local archive_dir=$1
+    local retention_days=$2
+    echo "Removing files older than $retention_days days from $archive_dir"
+    if [ ! "$PRETENDING" ]; then
+        find $archive_dir -name *.gz -mtime +${retention_days} -exec rm -v {} \;
+    elif [ -d "$archive_dir" ]; then
+        find $archive_dir -name *.gz -mtime +${retention_days}
+    fi
 }
 
 # Sync files from remote host to local directory. Requires key-based
 # ssh authentication.
 sync_files() {
-	if [ ! "$PRETENDING" ]; then
-		# set the environment up so we don't have to type our passphrase
-		eval $(ssh-agent)
-	else
-		local list_only_arg="--list-only"
-	fi
+    if [ ! "$PRETENDING" ]; then
+        # set the environment up so we don't have to type our passphrase
+        eval $(ssh-agent)
+    else
+        local list_only_arg="--list-only"
+    fi
 
-	if [ "$VERBOSE" ]; then
-		local verbose_arg="--verbose"
-	fi
-	
-	$RSYNC \
-		--archive \
-		--compress \
-		--rsh=ssh \
-		$list_only_arg \
-		$verbose_arg \
-		$REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR/ $LOCAL_DIR
+    if [ "$VERBOSE" ]; then
+        local verbose_arg="--verbose"
+    fi
+    
+    $RSYNC \
+        --archive \
+        --compress \
+        --rsh=ssh \
+        $list_only_arg \
+        $verbose_arg \
+        $REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR/ $LOCAL_DIR
 }
 
 #
@@ -256,45 +256,45 @@ sync_files() {
 
 # process command line args
 while getopts ":hcvpbfods" options; do
-	case $options in
-		h)
-			usage
-			exit 0
-			;;
-		c)
-			DO_PRINT_CONFIG=1
-			;;
-		v)
-			VERBOSE=1
-			;;
-		p)
-			PRETENDING=1
-			;;
-		b)
-			DO_FS_BACKUP=1
-			DO_DB_BACKUP=1
-			DO_BACKUP=1
-			;;
-		o)
-			DO_FORCE_FULL=1
-			;;
-		f)
-			DO_FS_BACKUP=1
-			DO_BACKUP=1
-			;;
-		d)
-			DO_DB_BACKUP=1
-			DO_BACKUP=1
-			;;
-		s)
-			DO_SYNC=1
-			;;
-		*)
-			echo "unknown option: $OPTARG"
-			usage
-			exit 1
-			;;
-	esac
+    case $options in
+        h)
+            usage
+            exit 0
+            ;;
+        c)
+            DO_PRINT_CONFIG=1
+            ;;
+        v)
+            VERBOSE=1
+            ;;
+        p)
+            PRETENDING=1
+            ;;
+        b)
+            DO_FS_BACKUP=1
+            DO_DB_BACKUP=1
+            DO_BACKUP=1
+            ;;
+        o)
+            DO_FORCE_FULL=1
+            ;;
+        f)
+            DO_FS_BACKUP=1
+            DO_BACKUP=1
+            ;;
+        d)
+            DO_DB_BACKUP=1
+            DO_BACKUP=1
+            ;;
+        s)
+            DO_SYNC=1
+            ;;
+        *)
+            echo "unknown option: $OPTARG"
+            usage
+            exit 1
+            ;;
+    esac
 done
 
 if [ "$DO_PRINT_CONFIG" ]; then
@@ -303,33 +303,33 @@ if [ "$DO_PRINT_CONFIG" ]; then
 fi
 
 if [ "$DO_BACKUP" ]; then
-	validate_env_vars
-	validate_directories
+    validate_env_vars
+    validate_directories
 
-	echo "Initiating backup at $(date)"
-	
-	if [ "$PRETENDING" ]; then
-		echo "Running in pretend mode. No commands will actually be executed."
-	fi
-	
-	if [ "$DO_FS_BACKUP" ]; then
-		backup_file_system
-	fi
-	
-	if [ "$DO_DB_BACKUP" ]; then
-		backup_database
-	fi
-	
-	remove_old_backups $ARCHIVE_DIR $REMOTE_RETENTION_DAYS
-	
-	echo "Backup completed at $(date)"
-	exit 0
+    echo "Initiating backup at $(date)"
+    
+    if [ "$PRETENDING" ]; then
+        echo "Running in pretend mode. No commands will actually be executed."
+    fi
+    
+    if [ "$DO_FS_BACKUP" ]; then
+        backup_file_system
+    fi
+    
+    if [ "$DO_DB_BACKUP" ]; then
+        backup_database
+    fi
+    
+    remove_old_backups $ARCHIVE_DIR $REMOTE_RETENTION_DAYS
+    
+    echo "Backup completed at $(date)"
+    exit 0
 fi
 
 if [ "$DO_SYNC" ]; then
-	sync_files
-	remove_old_backups $LOCAL_DIR $LOCAL_RETENTION_DAYS
-	exit 0
+    sync_files
+    remove_old_backups $LOCAL_DIR $LOCAL_RETENTION_DAYS
+    exit 0
 fi
 
 usage
